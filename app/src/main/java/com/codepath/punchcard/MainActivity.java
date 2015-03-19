a4331e57565c382a0625d347b7a3ee87a5d89fae
Harris Tsim <harris@squareup.com>
2015-03-19 00:12:42 -0700

    copy theming

--- app/src/main/java/com/codepath/punchcard/MainActivity.java
+++ app/src/main/java/com/codepath/punchcard/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CalendarView;
--- app/src/main/java/com/codepath/punchcard/activities/LoginActivity.java
+++ app/src/main/java/com/codepath/punchcard/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.punchcard.activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
