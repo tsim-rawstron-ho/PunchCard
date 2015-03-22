ad0d89f2ffc5363d97dc9db9480f93a9aad4cbd3
Harris Tsim <harris@squareup.com>
2015-03-22 00:05:44 -0700

    style with floating button

--- app/src/main/java/com/codepath/punchcard/MainActivity.java
+++ app/src/main/java/com/codepath/punchcard/MainActivity.java
@@ -26,6 +26,7 @@ import com.codepath.punchcard.models.User;
 import com.codepath.punchcard.models.Weather;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.melnykov.fab.FloatingActionButton;
 import com.parse.ParseUser;
 import com.roomorama.caldroid.CaldroidFragment;
 import com.roomorama.caldroid.CaldroidListener;
@@ -71,6 +72,15 @@ public class MainActivity extends ActionBarActivity implements  UpdateProfileFra
     weatherIcon.setTypeface(weatherFont);
     setupShiftList((Activity) this);
     setupCalendar(savedInstanceState);
+
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.attachToListView(shiftListView);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, CreateNewShiftActivity.class);
+        startActivity(intent);
+      }
+    });
   }
 
   private void setupCalendar(Bundle savedInstanceState) {
@@ -110,7 +120,7 @@ public class MainActivity extends ActionBarActivity implements  UpdateProfileFra
 
     selectedDate = date;
     loadShiftsFor(selectedDate);
-    caldroidFragment.setBackgroundResourceForDate(R.color.blue, selectedDate);
+    caldroidFragment.setBackgroundResourceForDate(R.color.pink, selectedDate);
     caldroidFragment.setTextColorForDate(R.color.white, selectedDate);
     caldroidFragment.refreshView();
 
@@ -134,11 +144,8 @@ public class MainActivity extends ActionBarActivity implements  UpdateProfileFra
 
   @Override public boolean onPrepareOptionsMenu(Menu menu) {
     MenuItem viewEmployee = menu.findItem(R.id.action_employees);
-    MenuItem createShift = menu.findItem(R.id.action_create_shift);
     User currentUser = (User) ParseUser.getCurrentUser();
     if (currentUser.isManager()) {
-      if (createShift != null)
-        createShift.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
       if (viewEmployee != null)
         viewEmployee.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
     }
@@ -180,9 +187,6 @@ public class MainActivity extends ActionBarActivity implements  UpdateProfileFra
         } else if (id == R.id.action_logout) {
             ParseUser.getCurrentUser().logOut();
             startActivity(new Intent(this, LoginActivity.class));
-        } else if (id == R.id.action_create_shift) {
-          Intent intent = new Intent(this, CreateNewShiftActivity.class);
-          startActivity(intent);
         } else if (id == R.id.action_profile) {
           Intent intent = new Intent(this, ProfileActivity.class);
           startActivity(intent);
